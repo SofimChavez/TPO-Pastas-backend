28018d5e6ce2515dfd7dff15b055f60e7344a0c0
SofimChavez <130805140+SofimChavez@users.noreply.github.com>
2023-07-07 17:18:58 -0300

    Modificaciones

--- pasta/pastas/settings.py
+++ pasta/pastas/settings.py
@@ -73,11 +73,23 @@ WSGI_APPLICATION = 'pastas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'SofiChavez$pastas',
+            'USER': 'SofiChavez',
+            'PASSWORD': 'TPOPastas',
+            'HOST': 'SofiChavez.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
 
 
